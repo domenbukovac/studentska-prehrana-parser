7d6be51912743c39178fea69eee2295ff364c646
Domen Bukovac <domen@bukovac.si>
2019-03-29 08:56:06 +0100

    first commit

---
+++ parse_menu.py
@@ -0,0 +1,85 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+from time import sleep
+import psycopg2
+import requests
+import datetime
+
+import json
+
+# load database config from config
+with open('config.json') as json_data_file:
+    database = json.load(json_data_file)["DATABASE"]
+print(database["user"])
+
+
+try: # database config
+    connect_str = f"dbname='{database['db_name']}' user='{database['user']}' host='{database['host']}' password='{database['password']}'"
+    conn = psycopg2.connect(connect_str)
+    cursor = conn.cursor()
+except Exception as e:
+    print(e)
+
+# request url
+url = "https://www.studentska-prehrana.si/sl/restaurant/MenuForDate"
+today = datetime.date.today()
+query = """INSERT INTO daily_menus_sp 
+        (restaurant_sp_id, title, tag, description, date) 
+        VALUES
+        (%s, %s, %s, %s, %s);"""
+
+
+cursor.execute("""SELECT sp_id, name FROM restaurants_sp""")
+weekday = today.weekday()
+
+# days left this week
+days_to_check =  7 - weekday
+
+# every restaurant id in database
+for restaurant in cursor.fetchall():
+    # rande of days
+    for i in range (0, days_to_check):
+        date = today + datetime.timedelta(days=i)
+        restaurant_sp_id = restaurant[0]
+
+        # check if any menu already exists on selected day for this restaurant
+        query2 = "SELECT id FROM daily_menus_sp WHERE date::date = '{}' AND restaurant_sp_id = {}".format(date, restaurant_sp_id)
+        cursor.execute(query2)
+
+
+        if (len(cursor.fetchmany(2)) == 0):
+            res = requests.post(url, data={'restaurantId': restaurant_sp_id, 'date': date})
+
+            page_html = res.text
+            page_soup = soup(page_html, "html.parser")
+
+            menus = page_soup.findAll(
+                'div', attrs={
+                    'class': 'shadow-wrapper'
+                })
+
+            for menu in menus:
+                title = menu.h5.strong.text.split("  ")[1].capitalize()
+                tag = None
+                try:
+                    tag = menu.find("img", attrs={"class" : "pull-right"}).attrs['title']
+                except:
+                    None
+                description = []
+
+                description_elements = menu.findAll("li")
+
+                for element in description_elements:
+                    description.append(element.i.text)
+                
+                description_string = "\n".join(description)
+                print(title)
+                print(description_string)
+                print(tag)
+                print("\n")
+                
+                data = (restaurant_sp_id, title, tag, description_string, date)
+                cursor.execute(query, data)
+                conn.commit()
+        else:
+            print("Already exists")
---
+++ parse_restaurants.py
@@ -0,0 +1,87 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+from time import sleep
+import psycopg2
+
+from decimal import Decimal
+
+import json
+
+with open('config.json') as json_data_file:
+    database = json.load(json_data_file)["DATABASE"]
+print(database["user"])
+
+
+url = "https://www.studentska-prehrana.si/restaurant/"
+
+uClient = uReq(url)
+page_html = uClient.read()
+uClient.close()
+
+page_soup = soup(page_html, "html.parser")
+restavracije = page_soup.findAll(
+    'div', attrs={
+        'class': 'restaurant-row'
+    })
+
+
+try:
+    connect_str = f"dbname='{database['db_name']}' user='{database['user']}' host='{database['host']}' password='{database['password']}'"
+    conn = psycopg2.connect(connect_str)
+    cursor = conn.cursor()
+
+    query = """INSERT INTO restaurants_sp 
+        (name, sp_id, street, zip, city, students_meal_price, latitude, longitude) 
+        VALUES
+        (%s, %s, %s, %s, %s, %s, %s, %s);"""
+
+    tag_query = """INSERT INTO restaurant_tags 
+        (restaurant_sp_id, name) 
+        VALUES
+        (%s, %s);"""
+except Exception as e:
+    print(e)
+
+vsi_atributi = []
+unikatni_atributi = []
+
+for restavracija in restavracije:
+
+  name =  restavracija.a.text.strip()
+  latitude = Decimal(restavracija.attrs['data-lat'])
+  longitude = Decimal(restavracija.attrs['data-lon'])
+  meal_price = Decimal(restavracija.attrs['data-doplacilo'].replace(",", "."))
+  sp_id =  restavracija.attrs['data-posid']
+  address = restavracija.attrs['data-naslov']
+  city = restavracija.attrs['data-city']
+
+  naslov = restavracija.small.i.text.split(", ")
+  kraj = naslov[1].split(" ")
+  zip = kraj[0]
+
+  print("Name: " + name)
+  print("Koordinates: " + str(longitude), str(latitude))
+  print("Bon: " + str(meal_price))
+  print("Id: " + sp_id)
+  print("Address: " + address)
+  print("City: " + city)
+
+  atributi = restavracija.find(
+    'div', attrs={
+        'class': 'pull-right'
+    })
+  slike = atributi.findAll('img')
+
+
+  data = (name, sp_id, address, zip, city, meal_price, latitude, longitude)
+  cursor.execute(query, data)
+  conn.commit()
+
+  for atribut in slike:
+    tag_name = atribut['title']
+
+    tag_data = (sp_id, tag_name)
+    cursor.execute(tag_query, tag_data)
+    conn.commit()
+
+  print("\n\n")
